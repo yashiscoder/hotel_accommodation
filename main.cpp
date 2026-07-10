252beba901d8c44160a8cf7f4baddb4a42e9c67b
yashiscoder <kumawatysh060@gmail.com>
2026-07-10 21:55:21 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,22 @@ const char* HOST = "localhost";
 const char* USER ="root";
 const char* PW =" ";
 const char* DB = "mydb";
+
+class Hostel{
+    private:
+    string name;
+    int bed, fees;
+}
 int main(){
+    MYSQL* conn;
+    conn = mysql_init(NULL);
+    if (!mysql_real_connect(conn, HOST, USER, PW, DB, 3306, NULL, 0)){
+        cout<<"Error: "<<mysql_error(conn)<<endl;
+    }
+    else{
+        cout<<"Connection success."<<endl;
+    }
+    Sleep(3000)
 
     return 0;
 }
